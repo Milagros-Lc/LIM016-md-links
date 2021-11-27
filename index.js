7393fc2492e504f526690ed6cdaf1acae8a7b005
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-11-27 12:45:52 -0500

    Search File

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ const path = require('path');
 console.log("Por favor, ingrese ruta");
 const ruta = process.openStdin();
 ruta.addListener("data", function (r) {
- const rutaConvert = r.toString();
- fs.readdir(rutaConvert.replace(/\r?\n|\r/g,"") , function (err,datos){
-  if (err) {
-    console.log("Ruta no encontrada",err);
-  }
-  console.log(datos.toString());  
- });
+  const rutaConvert = r.toString();
+  fs.readdir(rutaConvert.replace(/\r?\n|\r/g, ""), function (err, datos) {
+    if (err) {
+      console.log("Ruta no encontrada", err);
+    }
+    const arrayFiles = datos.toString().split(",");
+    console.log(arrayFiles);
+    if (arrayFiles.length > 1) {
+      for (let i = 0; i < arrayFiles.length; i++) {
+        fs.readdir(rutaConvert.replace(/\r?\n|\r/g, "") + "/" + arrayFiles[i].replace(/\r?\n|\r/g, ""), function (error, datos) {
+          if (error) {
+            console.log("Ruta no encontrada", error);
+          }
+          const arrayFiles2 = datos.toString().split(",");
+          console.log(arrayFiles2);
+          for (let i = 0; i < arrayFiles2.length; i++) {
+            const exten = path.extname(arrayFiles2[i]);
+            if (exten == ".md") {
+              console.log(arrayFiles2[i]);
+            }
+          }
+        });
+      }
+    }
+  });
 });
 
 
